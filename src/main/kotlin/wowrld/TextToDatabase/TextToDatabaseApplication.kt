a0cfc6d797a5d3cb7f37a654c834aeaeb8266ab4
Na <resurrection04@gmail.com>
2023-01-07 19:07:12 +0900

    init commit

---
+++ src/main/kotlin/wowrld/TextToDatabase/TextToDatabaseApplication.kt
@@ -0,0 +1,11 @@
+package wowrld.TextToDatabase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TextToDatabaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<TextToDatabaseApplication>(*args)
+}
---
+++ src/test/kotlin/wowrld/TextToDatabase/TextToDatabaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package wowrld.TextToDatabase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TextToDatabaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
